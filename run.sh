77a48667b75099008fec3336aab3231cd62f4750
Wes Saunders <wsaunders@coretechcorp.com>
2022-09-29 15:39:53 -0500

    Exposed port 443 in dockerfile Added default self-signed certs when needed

--- run.sh
+++ run.sh
@@ -2,6 +2,8 @@
 
 if [ -f "/certificates/fullchain.pem" ]; then 
     cp nginx/ssl.conf /certificates/ssl.conf 
+else
+    cp nginx/selfsignedsslcerts.conf /certificates/ssl.conf
 fi
 
 echo "Starting nginx"
