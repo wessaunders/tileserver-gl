02bcf92fbfe662c3497c3a9991e2e40606a91da8
Wes Saunders <wsaunders@coretechcorp.com>
2022-09-30 14:37:24 -0500

    update

--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-if [ -f "/certificates/fullchain.pem" ]; then 
-    cp ../app/ssl.conf /certificates/ssl.conf 
-else
-    cp ../app/selfsignedsslcerts.conf /certificates/ssl.conf
-fi
+# if [ -f "/certificates/fullchain.pem" ]; then 
+#     cp ./app/ssl.conf /certificates/ssl.conf 
+# else
+#     cp ./app/selfsignedsslcerts.conf /certificates/ssl.conf
+# fi
 
 echo "Starting nginx"
 nginx
 
-../app/docker-entrypoint.sh
+./app/docker-entrypoint.sh
